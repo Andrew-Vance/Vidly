cbfe3a0fac159beb864eabae8d8126c992508e07
Andrew Vance <andrewaddress2@gmail.com>
2021-02-13 11:46:31 -0800

    renamed server file to index

--- server/index.js
+++ server/index.js
@@ -5,5 +5,8 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/', express.static('dist');
+app.use('/', express.static('dist'));
 
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+})
