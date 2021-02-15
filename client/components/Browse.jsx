a1e644e7b08c74c128086ea96fe42acad1db498d
Andrew Vance <andrewaddress2@gmail.com>
2021-02-15 12:12:17 -0800

    implemented a Browse view for browsing uploaded videos

---
+++ client/components/Browse.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
+import axios from 'axios';
+import VideoEntry from './VideoEntry.jsx';
+
+const Browse = ({ changeView }) => {
+
+  const [videos, updateVideos] = useState([]);
+
+  useEffect(() => {
+    axios.get('/videos')
+    .then(results => {
+      updateVideos(results.data);
+    })
+  }, []);
+
+  return (
+     _.map(videos, video => {
+      return <li key={video.id}> < VideoEntry video={video} changeView={changeView} /> </li>;
+     })
+  );
+};
+
+export default Browse;
