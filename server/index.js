0b63027788efb6b2746ad8c2bc8f0fd6db4690e1
Andrew Vance <andrewaddress2@gmail.com>
2021-02-15 14:40:59 -0800

    added route for updating a videos description

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const fileUpload = require('express-fileupload');
 const port = process.env.PORT || 3000;
 const db = require('../database');
@@ -9,6 +10,37 @@ app.use(fileUpload());
 app.use(express.json());
 
 app.use('/', express.static('dist'));
+
+app.get('/video/:name', (req, res) => {
+  const path = `./files/${req.params.name}`
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+});
 
 app.get('/videos', (req, res) => {
   db.getAll()
@@ -51,6 +83,20 @@ app.post('/video', (req, res) => {
     })
 
   }
+});
+
+app.patch('/video/:name', (req, res) => {
+  let name = req.params.name;
+  let description = req.body.description;
+  console.log(req.body);
+  db.update(name, description)
+  .then(result => {
+    res.status(200).send('description updated');
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).send('error updating description');
+  })
 });
 
 app.listen(port, () => {
