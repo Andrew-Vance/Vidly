efc901a06d94d5f55c9f878699c27692b6fb636c
Andrew Vance <andrewaddress2@gmail.com>
2021-02-14 17:58:22 -0800

    implemented basic view system

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+
+  const [view, updateView] = useState('browse');
+  const [video, updateVideo] = useState(null);
+
+  const changeView = (newView, videoName = null) => {
+    updateView(newView);
+    updateVideo(videoName);
+  }
+
+  const renderView = () => {
+    if (view === 'video') {
+
+    } else if (view === 'browse') {
+
+    } else if (view === 'upload') {
+
+    }
+  }
+
   return (
     <div>
-      react works
+      <div className='header'>
+        <span className='nav' onClick={() => {changeView('Browse')}}>Browse</span>
+        <span className='nav' onClick={() => {changeView('Upload')}}>Upload</span>
+      </div>
     </div>
   );
 };
