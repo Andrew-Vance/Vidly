f6b0d9fcbce05d3c071f32af16c5cbe396831801
Andrew Vance <andrewaddress2@gmail.com>
2021-02-15 18:01:40 -0800

    added thumbnail img to entry

--- client/components/VideoEntry.jsx
+++ client/components/VideoEntry.jsx
@@ -4,7 +4,8 @@ const VideoEntry = ({ video }) => {
 
   return (
     <div>
-      <span>{video.name.slice(0, -4)}</span><br></br><br></br>
+      <img className='thumbnail' src={`http://localhost:3000/thumbnail/${video.name}`}></img>
+      <span className='title'>{video.name.slice(0, -4)}</span><br></br><br></br>
       <span className='description'>{video.description}</span>
     </div>
   );
