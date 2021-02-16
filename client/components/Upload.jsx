47d5434ae0a4e23b37a335826d50532c21847787
Andrew Vance <andrewaddress2@gmail.com>
2021-02-15 18:01:02 -0800

    added upload confirmation and ability to add description to video

--- client/components/Upload.jsx
+++ client/components/Upload.jsx
@@ -4,14 +4,18 @@ import axios from 'axios';
 const Upload = () => {
   const [file, updateFile] = useState(null);
   const [fileName, updateFileName] = useState('');
+  const [description, updateDescription] = useState('');
+  const [uploaded, updateUploaded] = useState(false);
 
   const handleChange = (e) => {
     let file = e.target.files[0];
     updateFile(file);
+    console.log(file);
   };
 
   const uploadFile = (e) => {
     e.preventDefault();
+    updateUploaded(false);
 
     if (file.type === 'video/mp4') {
       var formData = new FormData();
@@ -23,7 +27,14 @@ const Upload = () => {
         }
       })
       .then(response => {
-        console.log(response.data);
+        axios.patch(`/video/${fileName}`, {description})
+        .then(result => {
+          updateUploaded(true);
+        })
+        .catch(err => {
+          window.alert('error updating video descrption');
+          console.log(err);
+        });
       })
       .catch(err => {
         window.alert(err.response.data);
@@ -35,13 +46,25 @@ const Upload = () => {
 
   };
 
+  const renderUploaded = () => {
+    if (uploaded) {
+      return (
+        <div id='upload-confirmation'>
+          UPLOADED
+        </div>
+      );
+    }
+  };
+
   return (
     <div className='uploadView'>
       <form className='uploadForm' onSubmit={uploadFile}>
-      <div>Title of Video: <input type="text" name="title" onChange={(e)=>{updateFileName(e.target.value + '.mp4')}}/></div>
+      <div>Title of Video: <input className='titleInput' type="text" name="title" onChange={(e)=>{updateFileName(e.target.value + '.mp4')}}/></div>
+      Video Description: <input className='descriptionInput' type='text' onChange={(e) => {updateDescription(e.target.value)}}></input> <br></br>
       <input type="file" onChange={handleChange} />
       <input type="submit" value="Upload" />
       </form>
+      {renderUploaded()}
     </div>
   );
 };
