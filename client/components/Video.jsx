797a34fb318dd55542cb5ae65ba37e1cdf1f0821
Andrew Vance <andrewaddress2@gmail.com>
2021-02-16 17:17:42 -0800

    changed video src url to props.video.videoUrl

--- client/components/Video.jsx
+++ client/components/Video.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Video = ({ name }) => {
+const Video = ({ video }) => {
   return (
     <div id='videoView'>
       <video id='videoPlayer' controls>
-        <source src={`http://localhost:3000/video/${name}`} type='video/mp4' />
+        <source src={video.videoUrl} type='video/mp4' />
       </video>
     </div>
   )
