94e00744ba89c860db4b9d00752714d650f6f014
Andrew Vance <andrewaddress2@gmail.com>
2021-02-16 17:10:34 -0800

    updated schema to include video url and thumbnail url

--- schema.sql
+++ schema.sql
@@ -5,5 +5,7 @@ USE vidly;
 CREATE TABLE IF NOT EXISTS videos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(64),
-  description VARCHAR(124)
+  description VARCHAR(128),
+  videoUrl VARCHAR(128),
+  thumbURL VARCHAR(128)
 );
